55ee78537375f9b8f0b422cbfe164113005d7386
Peyton Warren <pwarren97@gmail.com>
2019-08-20 15:17:47 -0500

    Inital commit

---
+++ get_data/conf/conf.py
@@ -0,0 +1,11 @@
+# Configuration file containing relevant information
+
+
+
+# [ SQL ]
+sql_socket=""
+sql_username=""
+sql_password=""
+
+# [ Stock Data Source ]
+iex_token="pk_82f39a5069f646f08be56cb2f5d7eea7"
