f0e1919ab9ab00f36e54f142deaeef15d23b8b28
pwarren97 <pwarren97@gmail.com>
2019-08-29 18:20:30 -0500

    command line arguments are working

--- get_data/__main__.py
+++ get_data/__main__.py
@@ -1,16 +1,26 @@
 from os import sys
 import argparse
-
+import conf
 from sources import iex
+
+# parse the command line input
+parser = argparse.ArgumentParser("get_data.sh")
+parser.add_argument("-s", "--stock", nargs='+', type=str, help="what stock(s) to download. input should be ticker symbols")
+parser.add_argument("-d", "--date", nargs='+', type=str, help="takes start date or start and end dates for stock data in format yyyymmdd")
+args = parser.parse_args()
+print(args.stock)
+print(args.date)
+
 
 # Get the historical stock data from the internet
 stock_data = None
-if len(sys.argv) == 2:
-    # sys.argv[1] == ticker symbol name
-    stock_data = iex.get_stock_data(sys.argv[1], "20190101", "20190102")
-elif len(sys.argv) == 3:
-    stock_data = iex.get_stock_data(sys.argv[1], sys.argv[2])
 
+if len(args.date) == 1:
+    stock_data = iex.get_stock_data(args.stock, args.date[0])
+elif len(args.date) == 2:
+    stock_data = iex.get_stock_data(args.stock, args.date[0], args.date[1])
+else:
+    print("There either isn't a date or a stock.")
 
 # if stock_data.any():
 print(stock_data)
--- get_data/sources/iex.py
+++ get_data/sources/iex.py
@@ -12,23 +12,28 @@ def get_stock_data(ticker_symbol, start, end=None):
     """
     Returns historical stock data in a pandas object.
     Parameters must be in the form of strings.
-    Ticker symbol does not have to be case sensitive.
+    Ticker symbol does not have to be case sensitive, but it does have to be a list
 
     Returns columns: date open high low close volume
     """
     # Force all the types to be appropriate
-    if not isinstance(ticker_symbol, str):
-        raise TypeError("ticker_symbol must be a string.")
+    if not isinstance(ticker_symbol, list):
+        raise TypeError("ticker_symbol must be a list.")
     elif not isinstance(start, str):
         raise TypeError("start must be a string.")
     elif not isinstance(end, str) and not end == None:
         raise TypeError("end must be a string.")
 
+    for item in ticker_symbol:
+        if not isinstance(item, str):
+            raise TypeError("An item in ticker_symbol is not a string.")
+
     # Get the data from online
     if not end == None:
         stock_data = get_historical_data(ticker_symbol, start, end, output_format='pandas', token=conf.iex_token)
-    else:
+    elif int(start) < int(start):
         stock_data = get_historical_data(ticker_symbol, start, output_format='pandas', token=conf.iex_token)
+    
     return stock_data
 
 # def get_stock_data()
