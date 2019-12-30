8bef837d04c755f59d40f8c8b8cfc5d0125fce1f
pwarren97 <pwarren97@gmail.com>
2019-12-29 18:37:08 -0600

    test_get_data.sh had issues

--- test_get_data.sh
+++ test_get_data.sh
@@ -3,7 +3,7 @@
 # For testing to see if the entire program runs correctly
 # Test 1
 printf "Test 1:\n"
-echo "./get_data.sh -s tsla -d 20190102 20190104 --close_only"
+echo "./get_data.sh -s tsla -d 20190102 --close_only"
 ./get_data.sh -s tsla -d 20190102 --close_only
 
 # Test 2
@@ -15,7 +15,7 @@ echo "./get_data.sh -s aapl msft -d 20190102 20190104 --close_only"
 # Test 3
 printf "\nTest 3:\n"
 echo "./get_data.sh -s MSFT -d 20190102 20190104 --close_only"
-./get_data.sh -s MSFT -d 20190101 20190201 --close_only
+./get_data.sh -s MSFT -d 20190101 20190104 --close_only
 
 
 # # test cases that should error out
