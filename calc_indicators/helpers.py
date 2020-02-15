a234b9831f1ec5c5c480912871bb961aedd227d2
pwarren97 <pwarren97@gmail.com>
2020-02-15 10:43:34 -0600

    save some extra base template code

--- calc_indicators/helpers.py
+++ calc_indicators/helpers.py
@@ -1,6 +1,12 @@
 # This file has helpers for __main__.py
 from datetime import datetime
 
+indicator_names = \
+[
+"MACD:12-26-9",
+"MACD-Histogram:12-26-9"
+]
+
 def parse_start_and_end_dates(dates):
     year, month, day = parse_date(dates[0])
     start_date = datetime(year, month, day)
@@ -19,3 +25,9 @@ def parse_date(date):
     month = int(date[4:6])
     day = int(date[6:])
     return (year, month, day)
+
+def calc_indicators(indicators, start_date, end_date=None):
+    if not isinstance(indicators, list):
+        raise TypeError("The indicators must be passed in the from of a list.")
+    if not isinstance(indicators[0], str):
+        raise TypeError("The indicators must be in the form of a string.")
