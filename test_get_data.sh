d8333abe237b5df65555e6db2c0e19df92ccb247
pwarren97 <pwarren97@gmail.com>
2020-02-20 15:46:50 -0600

    Made some changes to test_get_data.sh so that it would work

--- test_get_data.sh
+++ test_get_data.sh
@@ -3,18 +3,18 @@
 # For testing to see if the entire program runs correctly
 # Test 1
 printf "Test 1:\n"
-echo "./get_data.sh -s tsla -d 20190102 --close_only"
-./get_data.sh -s tsla -d 20190102 --close_only
+echo "./stt get_data -s tsla -d 20190102 --close_only"
+./stt get_data -s tsla -d 20190102 --close_only
 
 # Test 2
 printf "\nTest 2:\n"
-echo "./get_data.sh -s MSFT -d 20190102 20190104 --close_only"
-./get_data.sh -s MSFT -d 20190101 20190104 --close_only
+echo "./stt get_data -s MSFT -d 20190102 20190104 --close_only"
+./stt get_data -s MSFT -d 20190101 20190104 --close_only
 
 # Test 3
 printf "\nTest 3:\n"
-echo "./get_data.sh -s aapl msft -d 20190102 20190104 --close_only"
-./get_data.sh -s aapl msft -d 20190102 20190104 --close_only
+echo "./stt get_data -s aapl msft -d 20190102 20190104 --close_only"
+./stt get_data -s aapl msft -d 20190102 20190104 --close_only
 
 # # test cases that should error out
 # ./get_data.sh
