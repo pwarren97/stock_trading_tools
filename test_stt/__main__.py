1c53befbc3744f844d5d7e8b44fce149e3e12d28
Peyton Warren <pwarren97@gmail.com>
2020-03-17 12:21:12 -0500

    fixed issue with tests, now all three run appropriately

--- test_stt/__main__.py
+++ test_stt/__main__.py
@@ -2,6 +2,7 @@ import unittest
 from stt_global_items import conf
 import pandas as pd
 from datetime import datetime
+import numpy as np
 
 if conf.DB == "mongodb":
     from stt_global_items.dbms.mongodb import Mongo as dbms
@@ -11,6 +12,7 @@ elif conf.DB == "sql":
 class TestDBMS(unittest.TestCase):
 
     # data is the same pulled from IEXCloud
+    # Note: Volume has to be a float, it errors with an integer, must see if there is an integer fix
     def setUp(self):
         """Gets called before every test case"""
         # Single stock Multiple date example
@@ -23,6 +25,16 @@ class TestDBMS(unittest.TestCase):
             "close": [157.92, 142.19, 148.26],
             "open": [154.89, 143.98, 144.53]
         }
+        # self.sing_stock_mult_date = pd.DataFrame(nd.array(
+        #     [[datetime(2019, 1, 2), datetime(2019, 1, 3), datetime(2019, 1, 4)],
+        #     ["AAPL", "AAPL", "AAPL"],
+        #     [37039737.0, 91312195.0, 58607070.0],
+        #     [158.85, 145.72, 148.55],
+        #     [154.23, 142, 143.8],
+        #     [157.92, 142.19, 148.26],
+        #     [154.89, 143.98, 144.53]],
+        #     dtype=[]
+        # ), columns=["date", "symbol", "volume", "high", "low", "close", "open"])
         # Single stock Single date example
         self.sing_stock_sing_date = {
             "date": [datetime(2019, 1, 2)],
@@ -34,19 +46,30 @@ class TestDBMS(unittest.TestCase):
             "open": [154.89]
         }
         # Multiple stock Multiple date example
-        # self.mult_stock_mult_date = {
-        #     "date": [datetime(2019, 1, 2), datetime(2019, 1, 3), datetime(2019, 1, 4), datetime(2019, 1, 2), datetime(2019, 1, 3), datetime(2019, 1, 4)],
-        #     "symbol": ["AAPL", "AAPL", "AAPL", "MSFT", "MSFT", "MSFT"],
-        #     "volume": [37039737.0, 91312195.0, 58607070.0],
-        #     "high": [158.85, 145.72, 148.55],
-        #     "low": [154.23, 142, 143.8],
-        #     "close": [157.92, 142.19, 148.26],
-        #     "open": [154.89, 143.98, 144.53]
-        # }
+        self.mult_stock_mult_date = {
+            "date": [datetime(2019, 1, 2), datetime(2019, 1, 3), datetime(2019, 1, 4), datetime(2019, 1, 2), datetime(2019, 1, 3), datetime(2019, 1, 4)],
+            "symbol": ["AAPL", "AAPL", "AAPL", "MSFT", "MSFT", "MSFT"],
+            "volume": [37039737.0, 91312195.0, 58607070.0, 35329345.0, 42578410.0, 44060620.0],
+            "high": [158.85, 145.72, 148.55, 101.75, 100.19, 102.51],
+            "low": [154.23, 142, 143.8, 98.94, 97.2, 98.93],
+            "close": [157.92, 142.19, 148.26, 101.12, 97.4, 101.93],
+            "open": [154.89, 143.98, 144.53, 99.55, 100.1, 99.72]
+        }
+        # Multiple stock Single date example
+        self.mult_stock_sing_date = {
+            "date": [datetime(2019, 1, 2), datetime(2019, 1, 1)],
+            "symbol": ["AAPL", "MSFT"],
+            "volume": [37039737.0, 35329345.0],
+            "high": [158.85, 101.75],
+            "low": [154.23, 98.94],
+            "close": [157.92, 101.12],
+            "open": [154.89, 99.55]
+        }
 
         self.test_df1 = pd.DataFrame(self.sing_stock_mult_date)
         self.test_df2 = pd.DataFrame(self.sing_stock_sing_date)
-        # self.test_df3 = pd.DataFrame(self.mult_stock_mult_date)
+        self.test_df3 = pd.DataFrame(self.mult_stock_mult_date)
+        self.test_df4 = pd.DataFrame(self.mult_stock_sing_date)
 
     def tearDown(self):
         """Gets called after every test case"""
@@ -60,10 +83,19 @@ class TestDBMS(unittest.TestCase):
         """Tests dbms.get_stock_data()"""
         dbms.save_stock_data(self.test_df1)
         df1 = dbms.get_stock_data(["AAPL"], datetime(2019, 1, 2), datetime(2019, 1, 4))
+        df1 = df1["AAPL"]
         df2 = dbms.get_stock_data(["AAPL"], datetime(2019, 1, 2))
+        df2 = df2["AAPL"]
+        df3 = dbms.get_stock_data(["AAPL", "MSFT"], datetime(2019, 1, 2), datetime(2019, 1, 4))
+        df3 = pd.concat([df3["AAPL"], df3["MSFT"]], ignore_index=True)
+        # pd.concat made the dtype of the volume column np.int64 and it should be np.float64 to match with the other dfs,
+        # that is why the following line has to be there
+        df3["volume"] = np.array(df3["volume"], dtype=np.float64)
 
-        self.assertTrue(self.test_df1.equals(df1['AAPL']))
-        self.assertFalse(self.test_df1.equals(df2['AAPL']))
+        # assert tests
+        self.assertTrue(self.test_df1.equals(df1))
+        self.assertFalse(self.test_df1.equals(df2))
+        self.assertTrue(self.test_df3.equals(df3))
 
     def test_save_symbols(self):
         """Tests dbms.save_stock_data()"""
