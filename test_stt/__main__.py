53585a37152144912b7cefcc813fe7bca3123283
Peyton Warren <pwarren97@gmail.com>
2020-03-19 10:59:36 -0500

    Merge with tests completed now

--- test_stt/__main__.py
+++ test_stt/__main__.py
@@ -42,12 +42,6 @@ class TestDBMS(unittest.TestCase):
             "volume": [37039737.0, 91312195.0, 58607070.0, 35329345.0, 42578410.0, 44060620.0],
             "high": [158.85, 145.72, 148.55, 101.75, 100.19, 102.51],
             "low": [154.23, 142, 143.8, 98.94, 97.2, 98.93],
-<<<<<<< HEAD
-            "close": [157.92, 142.19, 148.26, 101.12, 97.4, 98.93],
-            "open": [154.89, 143.98, 144.53, 99.55, 100.1, 99.72]
-        }
-
-=======
             "close": [157.92, 142.19, 148.26, 101.12, 97.4, 101.93],
             "open": [154.89, 143.98, 144.53, 99.55, 100.1, 99.72]
         }
@@ -61,15 +55,11 @@ class TestDBMS(unittest.TestCase):
             "close": [157.92, 101.12],
             "open": [154.89, 99.55]
         }
->>>>>>> tests
 
         self.test_df1 = pd.DataFrame(self.sing_stock_mult_date)
         self.test_df2 = pd.DataFrame(self.sing_stock_sing_date)
         self.test_df3 = pd.DataFrame(self.mult_stock_mult_date)
-<<<<<<< HEAD
-=======
         self.test_df4 = pd.DataFrame(self.mult_stock_sing_date)
->>>>>>> tests
 
     def tearDown(self):
         """Gets called after every test case"""
@@ -89,19 +79,7 @@ class TestDBMS(unittest.TestCase):
         df1 = df1["AAPL"]
 
         df2 = dbms.get_stock_data(["AAPL"], datetime(2019, 1, 2))
-<<<<<<< HEAD
-        df3 = dbms.get_stock_data(["AAPL", "MSFT"], datetime(2019, 1, 2), datetime(2019, 1, 4))
-        df3 = pd.concat([df3["AAPL"], df3["MSFT"]], ignore_index=True)
 
-
-        print(self.test_df3["volume"].equals(df3["volume"]))
-
-
-
-        self.assertTrue(self.test_df1.equals(df1['AAPL']))
-        self.assertFalse(self.test_df1.equals(df2['AAPL']))
-        self.assertTrue(self.test_df3.equals(df3))
-=======
         df2 = df2["AAPL"]
 
         df3 = dbms.get_stock_data(["AAPL", "MSFT"], datetime(2019, 1, 2), datetime(2019, 1, 4))
@@ -119,7 +97,6 @@ class TestDBMS(unittest.TestCase):
         self.assertFalse(self.test_df1.equals(df2))
         self.assertTrue(self.test_df3.equals(df3))
         self.assertTrue(self.test_df4.equals(df4))
->>>>>>> tests
 
     def test_save_symbols(self):
         """Tests dbms.save_stock_data()"""
