fd08969ebfc0ae08310206cd295320f92d4a0817
Peyton Warren <pwarren97@gmail.com>
2020-03-19 21:58:58 -0500

    worked on UI to try and make it cleaner

--- get_data/__main__.py
+++ get_data/__main__.py
@@ -15,10 +15,14 @@ elif conf.DB == "sql":
     from stt_global_items.dbms.sql import SQL as dbms
 
 
+# Specify the name of the options
+stock_option1, stock_option2 = "-s", "--stock"
+date_option1, date_option2 = "-d", "--date"
+
 # Parse the command line input
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--stock", nargs='+', type=str, help="what stock(s) to download. input should be ticker symbols")
-parser.add_argument("-d", "--date", nargs='+', type=str, help="takes start date or start and end dates for stock data in format yyyymmdd")
+parser.add_argument(stock_option1, stock_option2, nargs='+', type=str, help="what stock(s) to download. input should be ticker symbols")
+parser.add_argument(date_option1, date_option2, nargs='+', type=str, help="takes start date or start and end dates for stock data in format yyyymmdd")
 parser.add_argument("--symbols", help="download symbols if they are not in the database", action="store_true")
 parser.add_argument("--close_only", help="only get close prices", action="store_true")
 args = parser.parse_args()
@@ -28,22 +32,20 @@ args = parser.parse_args()
 if args.date:
     start_date, end_date = helpers.parse_start_and_end_dates(args.date)
 
+msg_part1 = "If you are aiming to pull stock data, you need to include"
+help_msg = "Use -h or --help to see the options."
+
 # Handle all the options
-if args.symbols and (args.date or args.stock): # Stock data and symbols can't be pulled at the same time
-    print("You cannot pull stock data and symbols at the same time. Use -h or --help to see the options.")
-elif args.symbols: # handle downloading symbols
+
+if args.symbols and not (args.stock or args.date): # handle downloading symbols
     symbols = source.get_symbols()
     dbms.save_symbols(symbols)
-elif args.stock == None or args.date == None: # handle not having date and stock options together
-    print("The stock (-s|--stock) and date (-d|--date) options are required. Use -h or --help to see the options.")
-else:
+elif args.stock and args.date and not args.symbols:
     # Get the historical stock data from the internet
     if end_date == None:
         stock_df = source.get_stock_data(args.stock, start_date, close_only=args.close_only)
     elif start_date < end_date:
         stock_df = source.get_stock_data(args.stock, start_date, end_date, close_only=args.close_only)
-    else:
-        print("There isn't a date, stock, or the dates aren't in order.")
 
     # Save data pulled from the database
     if "stock_df" in locals():
@@ -53,3 +55,14 @@ else:
         else:
             print("No new data to add to the database.")
         dbms.save_stock_data(stock_df)
+else:
+    if args.symbols and (args.stock or args.date): # Stock data and symbols can't be pulled at the same time
+        print("You cannot pull stock data and symbols at the same time. " + help_msg)
+    elif not args.date and not args.stock:
+        print(msg_part1 + " the stock (" + stock_option1 + "|" + stock_option2 + ") and date (" + date_option1 + "|" + date_option2 + ") optional parameters. " + help_msg)
+    elif args.date and not args.stock:
+        print(msg_part1 + " the stock (" + stock_option1 + "|" + stock_option2 + ") optional parameter. " + help_msg)
+    elif not args.date and args.stock:
+        print(msg_part1 + " the date (" + date_option1 + "|" + date_option2 + ") optional parameter. " + help_msg)
+    elif start_data >= end_date:
+        print("The start date must come before the end date.")
