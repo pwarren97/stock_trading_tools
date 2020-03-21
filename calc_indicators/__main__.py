4ce05b6a3b8aec854a0b9f73ce2c8d33ffff8245
Peyton Warren <pwarren97@gmail.com>
2020-03-21 18:29:02 -0500

    CHECKPOINT: Major UI updates

--- calc_indicators/__main__.py
+++ calc_indicators/__main__.py
@@ -11,6 +11,7 @@ elif conf.DB == "sql":
 
 # set up string variables to represent the optional parameters' names
 indicators_option1, indicators_option2 = "-i", "--indicators"
+all_indicators_option = "--all_indicators"
 list_indicators_option = "--list_indicators"
 
 # indicator sets
@@ -23,6 +24,7 @@ parser.add_argument(global_helpers.stock_option1, global_helpers.stock_option2, 
 parser.add_argument(global_helpers.date_option1, global_helpers.date_option2, nargs="+", type=str, help=global_helpers.date_param_help_msg)
 
 parser.add_argument(indicators_option1, indicators_option2, nargs="+", type=str, help="Select what indicators to use")
+parser.add_argument(all_indicators_option, help="Calculate all indicators at once", action='store_true')
 parser.add_argument(list_indicators_option, help="Lists all indicators that can be passed through", action="store_true")
 
 parser.add_argument(choose_set_option, nargs="+", type=str, help="Choose an indicator set to use just this time")
@@ -34,30 +36,40 @@ def print_available_indicators():
     for item in helpers.all_indicators:
         print("\t" + item)
 
+# Input validation
+valid_date = args.date and helpers.validate_date_param(args.date)
+valid_stock = args.stock and helpers.validate_stock_param(args.stock)
+valid_indicators = args.indicators and helpers.validate_ind_param(args.indicators)
+valid_sets = args.set and helpers.validate_set_param(args.set)
+
+
 # Handle options
-
-# Handle a request to generate indicators
-if args.date and args.stock and not (args.set or args.list_sets):
+# Handle a valid request to generate indicators
+if valid_date and valid_stock and (valid_sets ^ valid_indicators ^ args.all_indicators):
+    # Parse the dates and then pull the data from the db
     start_date, end_date = global_helpers.parse_start_and_end_dates(args.date)
+    db_data = dbms.get_stock_data(args.stock, start_date, end_date)
 
-    df = dbms.get_stock_data(args.stock, start_date, end_date)
+    # Set up the indicator generator
+    if args.set:
+        ind_gen = helpers.IndicatorGenerator(db_data, helpers.indicator_sets{args.set})
+    elif args.indicators:
+        ind_gen = helpers.IndicatorGenerator(db_data, indicators)
+    else:
+        ind_gen = helpers.IndicatorGenerator(db_data, helpers.all_indicators)
 
-    if not args.indicators and not args.all_indicators:
-        ind_gen = helpers.IndicatorGenerator()
-    if (args.indicators) and not
-        # Generate indicators
-        ind_gen = helpers.IndicatorGenerator(indicators)
-        if end_date == None:
-            indicator_df = ind_gen.calc_indicators(start_date)
-        elif start_date < end_date:
-            indicator_df = ind_gen.calc_indicators(start_date, end_date)
-        elif start_date > end_date:
-            print("The start date should come before the end date.")
+    if end_date == None:
+        indicator_df = ind_gen.calc_indicators(start_date)
+    elif start_date < end_date:
+        indicator_df = ind_gen.calc_indicators(start_date, end_date)
+    elif start_date > end_date:
+        print("The start date should come before the end date.")
 
     if indicator_df:
         dbms.save_indicators(indicator_df)
     else:
-        print("Something went wrong. #2")
+        print("No data was saved to the database")
+"""
 # Handle creating an indicator set
 elif args.indicators and not (args.date or args.stock):
     pass
@@ -78,3 +90,4 @@ else:
         print(msg_part1 + stock_msg + ". " + global_helpers.help_msg)
     elif (args.indicators or args.all_indicators):
         print(msg_part1 + date_msg + "or " + stock_msg + ". " + global_helpers.help_msg)
+"""
--- calc_indicators/helpers.py
+++ calc_indicators/helpers.py
@@ -1,34 +1,47 @@
 # This file has helpers for __main__.py
 import pandas as pd
 from datetime import datetime
-from indicators.model import Indicator
-
+# from indicators.model import Indicator
+# from indicator_sets import IndicatorSet
 
 all_indicators = \
 [
-"EMA:13",
-"EMA:200",
-"MACD:12-26-9", #MACD does histogram as well
-"RoC:7",
-"SRoC:13-7"
+"EMA",
+"MACD",
+"RoC",
+"SRoC"
 ]
 
+indicator_sets = {
+    "std1" :
+    [
+        # Indicators
+    ]
+}
+
+def validate_date_param(date):
+    return True
+
+def validate_stock_param(stock):
+    return True
+
+def validate_ind_param(indicators):
+    return True
+
+def validate_set_param(set):
+    return True
+
+
 class IndicatorGenerator():
-    def __init__(indicators=None):
-        if indicators==None:
-            if not isinstance(indicators, list):
-                raise TypeError("The indicators must be passed in the from of a list.")
-            elif not isinstance(indicators[0], str):
-                raise TypeError("The indicators must be in the form of a string.")
-            self.indicators = indicators
-        else:
-            # IN THE FUTURE THIS NEEDS TO BE THE DEFAULT SET
-            self.indicators = None
+    def __init__(stock_df, indicator_set):
+        if not isinstance(indicator_set, IndicatorSet):
+            raise TypeError("The indicator set passed through must be of type IndicatorSet")
+        self.indicator_set = indicator_set
 
     def calc_indicators(start_date, end_date=None):
         if not isinstance(start_date, datetime):
             raise TypeError("The start date must be in the form of a python datetime.datetime object.")
-        elif end_date is not None:
-            if not isinstance(end_date, datetime):
-                raise TypeError("The end date must be in the form of a python datetime.datetime object.")
+        elif end_date is not None and not isinstance(end_date, datetime):
+            raise TypeError("The end date must be in the form of a python datetime.datetime object.")
+
         return True
