b366da17c3a22bbd145afd614c28dbb5597be751
pwarren97 <pwarren97@gmail.com>
2020-05-03 18:54:43 -0500

    ROUND 1 of UI tests complete

--- calc_indicators/__main__.py
+++ calc_indicators/__main__.py
@@ -39,7 +39,7 @@ if valid_date and valid_stock and (valid_sets ^ valid_indicators ^ args.all_indi
     elif args.indicators:
         ind_gen = IndicatorGenerator(db_data, helpers.parse_indicators(args.indicators))
     else:
-        ind_gen = IndicatorGenerator(db_data, helpers.parse_indicators(helpers.all_indicator_args))
+        ind_gen = IndicatorGenerator(db_data, helpers.parse_indicators(helpers.all_indicators))
 
     # Generate indicators
     indicator_df = pd.DataFrame()
@@ -54,7 +54,6 @@ if valid_date and valid_stock and (valid_sets ^ valid_indicators ^ args.all_indi
         dbms.save_indicators(indicator_df)
     else:
         print("No data was saved to the database")
-"""
 # Handle errors
 else:
     msg_part1 = "You need to specify "
@@ -62,13 +61,12 @@ else:
     stock_msg = "the stock with (" + global_helpers.stock_option1 + "|" + global_helpers.stock_option2 + ")"
 
     if args.date and args.stock:
-        print(msg_part1 + "what indicators you want to use with (" + indicators_option1 + "|" + indicators_option2 + ") or " + all_indicator.args_option + ". " + global_helpers.help_msg)
+        print(msg_part1 + "what indicators you want to use with (" + helpers.indicators_option1 + "|" + helpers.indicators_option2 + ") or " + helpers.all_indicators_option + ". " + global_helpers.help_msg)
         print("")
         helpers.print_available_indicators()
     elif (args.indicators or args.all_indicators) and args.date:
-        print(msg_part1 + date_msg + ". "+ global_helpers.help_msg)
+        print(msg_part1 + stock_msg + ". "+ global_helpers.help_msg)
     elif (args.indicators or args.all_indicators) and args.stock:
-        print(msg_part1 + stock_msg + ". " + global_helpers.help_msg)
+        print(msg_part1 + date_msg + ". " + global_helpers.help_msg)
     elif (args.indicators or args.all_indicators):
         print(msg_part1 + date_msg + "or " + stock_msg + ". " + global_helpers.help_msg)
-"""
--- calc_indicators/helpers.py
+++ calc_indicators/helpers.py
@@ -7,7 +7,7 @@ from indicators.macd import MACD
 from stt_global_items import global_helpers
 import argparse
 
-all_indic_args = \
+all_indicators = \
 [
 "EMA",
 "MACD",
@@ -26,6 +26,16 @@ indicator_sets = {
     MACD(7, 36, 5)
     ]
 }
+
+# Define arguments specifically for calc-indicators
+indicators_option1, indicators_option2 = "-i", "--indicators"
+all_indicators_option = "--all_indicators"
+list_indicators_option = "--list_indicators"
+
+choose_set_option = "--set"
+list_indicator_sets_option = "--list_sets"
+
+
 
 def validate_ind_param(indicators):
     return True
@@ -64,15 +74,6 @@ def parse_arguments(parser):
     if not isinstance(parser, argparse.ArgumentParser):
         raise TypeError("You must pass through a argparse.ArgumentParser object.")
 
-    # set up string variables to represent the optional parameters' names
-    indicators_option1, indicators_option2 = "-i", "--indicators"
-    all_indicators_option = "--all_indicators"
-    list_indicators_option = "--list_indicators"
-
-    # indicator sets
-    choose_set_option = "--set"
-    list_indicator_sets_option = "--list_sets"
-
     # add arguments
     parser.add_argument(global_helpers.stock_option1, global_helpers.stock_option2, nargs="+", type=str, help=global_helpers.stock_param_help_msg)
     parser.add_argument(global_helpers.date_option1, global_helpers.date_option2, nargs="+", type=str, help=global_helpers.date_param_help_msg)
@@ -88,5 +89,5 @@ def parse_arguments(parser):
 def print_available_indicators():
     list_ind_msg = "The indicators available for use is as follows:"
     print(list_ind_msg)
-    for item in helpers.all_indicators:
+    for item in all_indicators:
         print(" "*5 + item)
