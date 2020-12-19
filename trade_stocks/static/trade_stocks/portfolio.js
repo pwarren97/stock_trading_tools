f9494e3926c1c7ac3711be900c328de20f30f029
pwarren97 <pwarren97@gmail.com>
2020-12-19 17:48:07 -0600

    Chart works, but apparently there is a trial version

--- trade_stocks/static/trade_stocks/portfolio.js
+++ trade_stocks/static/trade_stocks/portfolio.js
@@ -4,13 +4,13 @@
 window.onload = function () {
   var dataPoints = [{
     x: new Date(),
-    y: 58.0
+    y: 508.0
   }];
 
   var chart = new CanvasJS.Chart("portfolioChart", {
     animationEnabled: true,
     theme: "light2",  // "light1", "light2", "dark1", "dark2",
-    exportEnabled: true,
+    exportEnabled: false,
     title: {
       text: "Portfolio"
     },
@@ -27,11 +27,11 @@ window.onload = function () {
     },
     toolTip: {
       content: ""
-    }.
+    },
     data: [{
       type: "candlestick",
       yValueFormatString: "$##0.00",
-      dataPoints: dataPoints;
+      dataPoints: dataPoints
     }]
   });
 
