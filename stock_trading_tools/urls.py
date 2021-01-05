ff81ba5b7c1fa10009a6b97df57b78a1eb8edcef
pwarren97 <pwarren97@gmail.com>
2021-01-05 10:45:11 -0600

    Moving back to using login app instead of builtin django things

--- stock_trading_tools/urls.py
+++ stock_trading_tools/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 # from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # path('', include('login.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('login.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('trade/', include('trade_stocks.urls')),
     path('admin/', admin.site.urls),
     # path('change-password/', auth_views.PasswordChange.as_view())
